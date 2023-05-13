2d558465387826a9c66d7624340e4dd88eb392b3
LMJ <leemj6825@naver.com>
2023-05-14 02:13:05 +0900

    chap03 ExpiryDateCalculator test code 작성 - 11: refactoring

--- src/test/java/com/study/tdd/chap03/ExpiryDateCalculator.java
+++ src/test/java/com/study/tdd/chap03/ExpiryDateCalculator.java
@@ -5,16 +5,34 @@ import java.time.YearMonth;
 
 public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(PayData payData) {
-        int addedMonth = payData.getPayAmount() / 10_000;
+        int addedMonths = payData.getPayAmount() / 10_000;
         if (payData.getFirstBillingDate() != null) {
-            LocalDate expectedNextBillingDate = payData.getBillingDate().plusMonths(addedMonth);
-            if (payData.getFirstBillingDate().getDayOfMonth() != expectedNextBillingDate.getDayOfMonth()) {
-                if (YearMonth.from(expectedNextBillingDate).lengthOfMonth() < payData.getFirstBillingDate().getDayOfMonth()) {
-                    return expectedNextBillingDate.withDayOfMonth(YearMonth.from(expectedNextBillingDate).lengthOfMonth());
-                }
-                return expectedNextBillingDate.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
-            }
+            return expiryDateUsingFirstBillingDate(payData, addedMonths);
+        } else {
+            return payData.getBillingDate().plusMonths(addedMonths);
         }
-        return payData.getBillingDate().plusMonths(addedMonth);
+    }
+
+    private LocalDate expiryDateUsingFirstBillingDate(PayData payData, int addedMonth) {
+        LocalDate expectedNextBillingDate = payData.getBillingDate().plusMonths(addedMonth);
+        final int dayOfFirstBillingDate = payData.getFirstBillingDate().getDayOfMonth();
+
+        if (isSameDayOfMonth(expectedNextBillingDate, dayOfFirstBillingDate)) {
+            final int dayLengthOfExpectedNextBillingDate = lastDayOfMonth(expectedNextBillingDate);
+            if (dayLengthOfExpectedNextBillingDate < dayOfFirstBillingDate) {
+                return expectedNextBillingDate.withDayOfMonth(dayLengthOfExpectedNextBillingDate);
+            }
+            return expectedNextBillingDate.withDayOfMonth(dayOfFirstBillingDate);
+        } else {
+            return expectedNextBillingDate;
+        }
+    }
+
+    private boolean isSameDayOfMonth(LocalDate expectedNextBillingDate, int dayOfFirstBillingDate) {
+        return dayOfFirstBillingDate != expectedNextBillingDate.getDayOfMonth();
+    }
+
+    private int lastDayOfMonth(LocalDate expectedNextBillingDate) {
+        return YearMonth.from(expectedNextBillingDate).lengthOfMonth();
     }
 }
